102aa8962e6a7229be3acddf0426d5c811d7e759
jaffasunny <jaffer.sunny125@gmail.com>
2021-06-18 16:46:34 +0500

    Todo UI

---
+++ app.js
@@ -0,0 +1,37 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+
+
+// Funcitons
+function addTodo(event){
+    // Prevent form from submitting
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Completed or Checked Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    // Trash or Delete Button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class = "fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //  APPEND TO LIST
+    todoList.appendChild(todoDiv);
+    // Clear Todo INPUT Value
+    todoInput.value = "";
+}
